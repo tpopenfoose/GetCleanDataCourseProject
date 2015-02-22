54afa140b06c63a6944a040665ea5c752a1efa88
tpopenfoose <tpopenfoose@earthlink.net>
2015-02-22 16:51:15 -0500

    cleaned up comments in run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,120 +1,88 @@
 library(dplyr, warn.conflicts=FALSE)
 
-# see what is there
+## see what is there
+## assume all the data is in the directory of "./data/UCI HAR Dataset/" from the working directory
 list.files("./data", recursive=TRUE)
 
 
-##########################################################################################
-
-ActivityLabels <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
-names(ActivityLabels) <- c("ActivityID", "Activity")
-ActivityLabels
-
+## read in the feature labels (will be used for both test and training data)
 FeatureLabels <- read.table("./data/UCI HAR Dataset/features.txt")
 names(FeatureLabels) <- c("FeatureID", "Feature")
-str(FeatureLabels)
 
-FeatureLabels %>% filter(grepl("-mean\\()|-std\\()", Feature))
 
-###########################################################################################
-
+## read in the obvservations for the test data
 TestDataSet <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+## now name the test data variables using the feature labels
 names(TestDataSet) <- FeatureLabels$Feature
-names(TestDataSet)
-str(TestDataSet)
-summary(TestDataSet)
-dim(TestDataSet)
-head(TestDataSet)
 
+## read in the activity label for each observation of the test data
 TestActivityLabelCode <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
-head(TestActivityLabelCode)
-dim(TestActivityLabelCode)
-summary(TestActivityLabelCode)
-unique(TestActivityLabelCode)
-table(TestActivityLabelCode, useNA="ifany")
-dim(TestActivityLabelCode)
+## name the ActivityID variable of the TestActivityLabels
 names(TestActivityLabelCode) <- "ActivityID"
 
+## read in the code for which subject was associated with each test observation
 TestSubjectCode <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
-table(TestSubjectCode, useNA="ifany")
-dim(TestSubjectCode)
-unique(TestSubjectCode)
-dim(TestSubjectCode)
-head(TestSubjectCode)
+## name the SubjectID variable of the TestSubjectsCodes
 names(TestSubjectCode) <- "SubjectID"
 
-names(TestDataSet)
-head(data.frame(TestSubjectCode, TestActivityLabelCode, TestDataSet))
-##########################################################################################
 
+## read in the obvservations for the training data
 TrainDataSet <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+## now name the training data variables using the feature labels
 names(TrainDataSet) <- FeatureLabels$Feature
-names(TrainDataSet)
-str(TrainDataSet)
-summary(TrainDataSet)
 
+## read in the activity label for each observation of the training data
 TrainActivityLabelCode <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
-head(TrainActivityLabelCode)
-dim(TrainActivityLabelCode)
-summary(TrainActivityLabelCode)
-unique(TrainActivityLabelCode)
-table(TrainActivityLabelCode, useNA="ifany")
+## name the ActivityID variable of the TrainActivityLabels
 names(TrainActivityLabelCode) <- "ActivityID"
 
+## read in the code for which subject was associated with each training observation
 TrainSubjectCode <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
-table(TrainSubjectCode, useNA="ifany")
-dim(TrainSubjectCode)
-unique(TrainSubjectCode)
+## name the SubjectID variable of the TrainSubjectsCodes
 names(TrainSubjectCode) <- "SubjectID"
 
-TrainDataSet$Subject=TrainSubjectCode
-TrainDataSet$ActivityLabelCode=TrainActivityLabelCode
-names(TrainDataSet)
 
-##########################################################################################
 
+## 1. merge the test and traing data sets to make one data set
 DataSet <- rbind(data.frame(TestSubjectCode, TestActivityLabelCode, TestDataSet),
                  data.frame(TrainSubjectCode, TrainActivityLabelCode, TrainDataSet))
-dim(DataSet)
+
+
+## 2. extract only the mean and std of each variable observation
+DataSet <- DataSet %>% select(SubjectID, ActivityID, matches("*\\.mean\\.|*\\.std\\."))
+
+
+## read in the activity labels
+ActivityLabels <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
+## name the activity variable labels
+names(ActivityLabels) <- c("ActivityID", "Activity")
+## 3. merge the activity labels to the main data set
 DataSet <- merge(DataSet, ActivityLabels)
-head(DataSet)
-##########################################################################################
 
-dim(DataSet)
 
-newDF <- DataSet %>% select(SubjectID, Activity, matches("*\\.mean\\.|*\\.std\\.")); names(newDF)
-head(newDF)
-summary(newDF)
-unique(names(newDF))
-names(newDF)
-sum(duplicated(names(newDF)))
-
-varNames <- names(newDF)[-(1:2)]
-varNames
+## 4. appropriately label the data set with descriptive variable names
+## get the current feature variable names
+varNames <- names(DataSet)[-(1:2)]
+## change leading t to Time (for time based variables)
 varNames <- sub("^t", "Time", varNames)
+## change leading f to Freq (for frequency based variables)
 varNames <- sub("^f", "Freq", varNames)
+## clean up the doubled
 varNames <- sub("BodyBody", "Body", varNames)
+## finish cleaning
 varNames <- sub(".mean..", "Mean", varNames)
 varNames <- sub(".std..", "Std", varNames)
 varNames <- sub(".X", "X", varNames)
 varNames <- sub(".Y", "Y", varNames)
 varNames <- sub(".Z", "Z", varNames)
-names(newDF)[-(1:2)] <- varNames
+## now rename the data set with descriptive variable names
+names(DataSet)[-(1:2)] <- varNames
 
-#########################################################################################
-dim(newDF)
-IndTidyData <- newDF %>% group_by(SubjectID, Activity) %>% summarise_each(funs(mean))
+
+## 5. create independent tidy data set with average of each variable for each activity and each subject
+IndTidyData <- DataSet %>% select(-matches("ActivityID")) %>%
+    group_by(SubjectID, Activity) %>% summarise_each(funs(mean))
+
+
+## 6. write table of the independent tidy data to text file
 write.table(IndTidyData, "IndependentTidyData.txt", row.name=FALSE)
-dim(IndTidyData)
-unique(IndTidyData$SubjectID)
-unique(IndTidyData$Activity)
-summary(IndTidyData)
-
-##########################################################################################
-
-df <- read.table("IndependentTidyData.txt", header = TRUE)
-View(df)
-dim(df)  # 180 68
-unique(df[ , 1])  # 1:30
-unique(df[ , 2])  # Levels: LAYING SITTING STANDING WALKING WALKING_DOWNSTAIRS WALKING_UPSTAIRS
-names(df)
